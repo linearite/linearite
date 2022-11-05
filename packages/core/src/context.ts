e13fd92207f4a15cf19e750b77c60126ee451ee5
yijie4188 <yijie4188@gmail.com>
2022-11-05 17:27:43 +0800

    feat(core): Context Events

--- packages/core/src/context.ts
+++ packages/core/src/context.ts
@@ -3,7 +3,13 @@ import * as cordis from 'cordis'
 import Linearite from './index'
 import { Builder } from './builder'
 
-export interface Context<B> {}
+export interface Events<C extends Context<Builder.Types> = Context<Builder.Types>> extends cordis.Events<C> {
+}
+
+export interface Context<B> {
+  [Context.config]: Context.Config<B>
+  [Context.events]: Events<this>
+}
 
 export class Context<B extends Builder.Types> extends cordis.Context<Context.Config<B>> {
   constructor(options?: Context.Config<B>) {
