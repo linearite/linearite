7ba0a72d3d6187c6cf0a8127020c2d4a7797c275
yijie4188 <yijie4188@gmail.com>
2022-11-08 23:04:02 +0800

    feat(core): auto register build plugin

--- packages/core/src/context.ts
+++ packages/core/src/context.ts
@@ -12,7 +12,8 @@ export type Plugin<N extends Plugin.Names> = {
   call: (ctx: Context<N>) => void
 } | {
   type: 'builder'
-  call: (ctx: Context<N>, opts: Builder.Opts & Plugin.Confs[N]) => void
+  conf: Omit<Builder.Opts, 'type'> & Plugin.Confs[N]
+  call: (ctx: Context<N>, opts: Omit<Builder.Opts, 'type'> & Plugin.Confs[N]) => void
 })
 
 export const definePlugin = <N extends Plugin.Names>(plugin: Plugin<N>) => plugin
@@ -20,6 +21,35 @@ export const definePlugin = <N extends Plugin.Names>(plugin: Plugin<N>) => plugi
 export namespace Plugin {
   export interface Confs extends Builder.Confs {}
   export type Names = keyof Confs
+  function isPlugin(v: any): v is Plugin<Names> {
+    return v
+      && typeof v === 'object'
+      && 'type' in v
+      && 'name' in v
+      && 'call' in v
+  }
+  export function r<N extends Plugin.Names>(name: N) {
+    const prefixArr = ['@linearite/plugin-', 'linearite-plugin-', '']
+    let module: Plugin<N> | undefined
+    let error: Error | undefined
+    for (const prefix of prefixArr) {
+      try {
+        module = require(`${prefix}${name}`).default
+        if (!isPlugin(module)) {
+          module = undefined
+          // noinspection ExceptionCaughtLocallyJS
+          throw new Error(`Plugin ${name} is not valid`)
+        }
+        break
+      } catch (e) {
+        error = e
+      }
+    }
+    if (!module) {
+      throw error
+    }
+    return module
+  }
 }
 
 export interface Events<C extends Context<Builder.Types> = Context<Builder.Types>> extends cordis.Events<C> {
@@ -36,6 +66,42 @@ export class Context<B extends Builder.Types> extends cordis.Context<Context.Con
     options?: Context.Config<B>,
   ) {
     super(options)
+    options.builder && this.initBuilder(program, options)
+  }
+
+  initBuilder(
+    program: Command,
+    options?: Context.Config<B>,
+  ) {
+    let builderOpts: string | Builder.Opts & Builder.Confs[B]
+    if (Linearite.isInherit(options.builder)) {
+      builderOpts = 'esbuild'
+    } else {
+      if (
+        typeof options.builder === 'string'
+        || typeof options.builder === 'object'
+      ) {
+        builderOpts = options.builder
+      }
+    }
+    if (typeof builderOpts === 'string') {
+      const plugin = Plugin.r(builderOpts as Builder.Types)
+      if (plugin.type !== 'builder') {
+        throw new Error(`plugin ${builderOpts} is not a builder`)
+      }
+      plugin.call(this, plugin.conf)
+    }
+    if (typeof builderOpts === 'object') {
+      const plugin = Plugin.r(builderOpts.type)
+      if (plugin.type !== 'builder') {
+        throw new Error(`plugin ${builderOpts.type} is not a builder`)
+      }
+      plugin.call(this, builderOpts)
+    }
+    program.command('build')
+      .action(() => {
+        console.log('build')
+      })
   }
 
   regiter(plugin: Plugin<any>) {
