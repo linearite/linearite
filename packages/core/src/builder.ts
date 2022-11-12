9e62cfd1c7c5cc0cce2c65bd6cf3e302ac983fd7
yijie4188 <yijie4188@gmail.com>
2022-11-12 14:43:22 +0800

    feat(core): infer builder name micro type function

--- packages/core/src/builder.ts
+++ packages/core/src/builder.ts
@@ -1,3 +1,5 @@
+import { Plugin } from './context'
+
 export namespace Builder {
   export type Format = 'cjs' | 'esm' | 'iife' | 'umd'
   /**
@@ -11,6 +13,8 @@ export namespace Builder {
    */
   export interface Confs {}
   export type Types = keyof Confs
+  export type InferName<N extends Plugin.Names> = N extends `builder-${infer B extends Types}`
+    ? B : never
   export interface Opts {
     type: Types
     target: string | string[]
