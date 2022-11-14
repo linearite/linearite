6a4123b11c0e97c2a53eefa86918d7320d928cfa
yijie4188 <yijie4188@gmail.com>
2022-11-14 23:19:43 +0800

    chore: update overides demo

--- .linearite.config.ts
+++ .linearite.config.ts
@@ -1,6 +1,13 @@
 export default {
   builder: 'esbuild',
-  depMap: {
-    cli: ['core']
-  }
+  overides: [{
+    rules: '@linerite/cli',
+    builder: {
+      define: {
+        PKG_NAME: 'linearite',
+        PKG_VERSION: '0.0.0',
+        PKG_DESCRIPTION: 'linearite is a tool for building monorepo'
+      },
+    },
+  }],
 }
