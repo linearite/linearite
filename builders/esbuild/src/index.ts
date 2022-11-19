6d989e08b497876091d538fc49c66216f664e3ce
yijie4188 <yijie4188@gmail.com>
2022-11-19 23:40:51 +0800

    feat(builders/esbuild): support build workspace

--- builders/esbuild/src/index.ts
+++ builders/esbuild/src/index.ts
@@ -1,3 +1,6 @@
+import path from 'path'
+
+import { build } from 'esbuild'
 import { definePlugin } from '@linearite/core'
 
 declare module '@linearite/core' {
@@ -13,14 +16,39 @@ export default definePlugin({
   name: 'builder-esbuild',
   conf: {
     target: ['node12'],
-    format: ['umd', 'esm'],
+    format: ['esm', 'cjs'],
   },
   call: (ctx, conf) => {
-    ctx.on('build:item', (workspace, opts) => {
-      console.log(
-        [...ctx.workspaces]
-      )
-      console.log('build:item', workspace, opts)
+    ctx.on('build:item', async (workspace, opts) => {
+      console.log('> build:item', workspace.meta.name, opts, conf)
+      const format = Array.isArray(conf.format) ? conf.format : [conf.format]
+      function dir(...paths: string[]) {
+        return path.join(workspace.path, ...paths)
+      }
+      await Promise.all(format.map(async (format) => {
+        if (format === 'umd')
+          throw new Error('esbuild not support umd format')
+
+        if (format === 'esm' && !workspace.meta.module) {
+          console.warn('not found module field in package.json, will fallback to `${outdir}/index.mjs`')
+        }
+        if (format === 'cjs' && !workspace.meta.main) {
+          console.warn('not found main field in package.json, will fallback to `${outdir}/index.js`')
+        }
+        await build({
+          entryPoints: [dir('src/index.ts')],
+          outfile: {
+            esm: dir(workspace.meta.module ?? 'dist/index.mjs'),
+            cjs: dir(workspace.meta.main   ?? 'dist/index.cjs'),
+            iife: dir(workspace.meta.main.replace(/\.js$/, '.iife.js')),
+          }[format],
+          bundle: true,
+          target: conf.target,
+          platform: 'node',
+          format,
+          external: Object.keys(workspace.meta.dependencies || {}),
+        })
+      }))
     })
   }
 })
