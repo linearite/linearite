dc9a923fba6ac913ffa8feef258e94d88d7971c8
yijie4188 <yijie4188@gmail.com>
2022-11-20 00:14:36 +0800

    feat(builders/esbuild): support output dir prop

--- builders/esbuild/src/index.ts
+++ builders/esbuild/src/index.ts
@@ -30,17 +30,20 @@ export default definePlugin({
           throw new Error('esbuild not support umd format')
 
         if (format === 'esm' && !workspace.meta.module) {
-          console.warn('not found module field in package.json, will fallback to `${outdir}/index.mjs`')
+          console.warn(`not found module field in package.json, will fallback to \`${conf.outdir}/index.mjs\``)
         }
         if (format === 'cjs' && !workspace.meta.main) {
-          console.warn('not found main field in package.json, will fallback to `${outdir}/index.js`')
+          console.warn(`not found main field in package.json, will fallback to \`${conf.outdir}/index.js\``)
         }
         await build({
           entryPoints: [dir('src/index.ts')],
           outfile: {
-            esm: dir(workspace.meta.module ?? 'dist/index.mjs'),
-            cjs: dir(workspace.meta.main   ?? 'dist/index.cjs'),
-            iife: dir(workspace.meta.main.replace(/\.js$/, '.iife.js')),
+            esm: dir(workspace.meta.module
+              ?? `${conf.outdir}/index.mjs`),
+            cjs: dir(workspace.meta.main
+              ?? `${conf.outdir}/index.cjs`),
+            iife: dir(workspace.meta.main?.replace(/\.js$/, '.iife.js')
+              ?? `${conf.outdir}/index.iife.js`),
           }[format],
           bundle: true,
           target: conf.target,
