9ffd30a51fdb31b86be37bbb2656209191a647dc
yijie4188 <yijie4188@gmail.com>
2022-11-19 22:30:37 +0800

    feat(cli): fallback no selected workspaces

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,6 +2,7 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
+import inquirer from 'inquirer'
 import { Command } from 'commander'
 import Linearite, { Context, Plugin } from '@linearite/core'
 
@@ -110,8 +111,47 @@ async function main() {
     })
 
   context
-    .on('build', (opts) => {
-      return Promise.all(workspaces?.map(async workspace => {
+    .on('build', async (opts) => {
+      let curWorkspaces = workspaces
+      if (curWorkspaces.length === 0) {
+        const allWorkspaces = [...store]
+        if (opts.all) {
+          curWorkspaces = allWorkspaces
+        } else {
+          let choices = [ 'all' ].concat(allWorkspaces.map(({ meta }) => meta.name))
+          const result = await inquirer.prompt<{
+            workspaces: string[]
+          }>([{
+            type: 'checkbox',
+            loop: false,
+            name: 'workspaces',
+            message: 'select target workspaces',
+            validate(input: string[]) {
+              if (input.length > 1 && input.includes('all')) {
+                return 'you can only select all or other workspaces'
+              }
+              return input.length > 0
+            },
+            choices
+          }])
+          if (result.workspaces.includes('all')) {
+            curWorkspaces = allWorkspaces
+          } else {
+            curWorkspaces = result.workspaces
+              .map(w => store[w])
+              .flat()
+          }
+        }
+      } else {
+        if (opts.all) {
+          console.warn('ignore --all option')
+        }
+      }
+      if (curWorkspaces.length === 0) {
+        console.warn('no workspaces to build')
+        return
+      }
+      return Promise.all(curWorkspaces?.map(async workspace => {
         await context.parallel('build:item', workspace, opts)
       }))
     })
