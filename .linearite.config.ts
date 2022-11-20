a2b34faf58b46eb63d8cdfb566c9edd8f813fd3d
yijie4188 <yijie4188@gmail.com>
2022-11-20 21:09:22 +0800

    chore: update line configuration

--- .linearite.config.ts
+++ .linearite.config.ts
@@ -1,13 +1,17 @@
+/** @type {import('@linearite/core').Linearite.Configuration} */
 export default {
-  builder: 'esbuild',
-  overides: [{
-    rules: '@linerite/cli',
-    builder: {
-      define: {
-        PKG_NAME: 'linearite',
-        PKG_VERSION: '0.0.0',
-        PKG_DESCRIPTION: 'linearite is a tool for building monorepo'
-      },
-    },
-  }],
+  builder: {
+    type: 'esbuild',
+    platform: 'node'
+  },
+  // overides: [{
+  //   rules: '@linerite/cli',
+  //   builder: {
+  //     define: {
+  //       PKG_NAME: 'linearite',
+  //       PKG_VERSION: '0.0.0',
+  //       PKG_DESCRIPTION: 'linearite is a tool for building monorepo'
+  //     },
+  //   },
+  // }],
 }
