58ca03306d4abfd17352aba9067893488ff73d2c
yijie4188 <yijie4188@gmail.com>
2023-02-11 23:52:48 +0800

    fix(cli): unable loading target builder conf

--- packages/cli/src/overides.ts
+++ packages/cli/src/overides.ts
@@ -1,6 +1,7 @@
 import Linearite, { Context, Plugin, resolveBuilderOpts } from '@linearite/core'
 import { merge, omit } from './utils'
 import minimatch from 'minimatch'
+import { removeConfKeys } from './inner/use-plugins'
 
 type CalcBuilderConf =
   & Linearite.Configuration<Plugin.Names>
@@ -22,13 +23,13 @@ export class OveridesService {
       scopes = scope
 
     const overideConfigs: Linearite.Configuration<Plugin.Names>[] = []
-    this.matrix.forEach(link => {
+    this.matrix.forEach((link, i) => {
       const conf = link.reduce((acc, [keys, computeConf]) => {
         // TODO 非第一个元素的其他矩阵组需要合并根的配置
         if (keys.length === 0) {
           return acc
         }
-        return keys.reduce((acc, key) => {
+        const weight = keys.reduce((acc, key) => {
           // TODO 处理其他的匹配模式，再将 use plugins 的逻辑和次数的统一维护
           return acc + ([
             ...scopes.map(scope => `@${scope}/${key}`),
@@ -36,12 +37,13 @@ export class OveridesService {
           ].reduce((b, glob) => b || minimatch(workspace.meta.name, glob), false)
             ? 2
             : 0)
-        }, 0) > 0
-          ? merge(acc, omit(computeConf, Linearite.InnerConfKeys))
-          : acc
+        }, 0)
+        return weight <= 0 ? acc : merge(acc, omit(computeConf, removeConfKeys))
       }, {})
       if (Object.keys(conf).length > 0) {
-        overideConfigs.push(conf)
+        overideConfigs.push(
+          i === 0 ? conf : Object.assign(omit(this.ctx.config, removeConfKeys), conf)
+        )
       }
     })
     return overideConfigs
