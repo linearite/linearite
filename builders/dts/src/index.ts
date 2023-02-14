a61569dfb1b11291d1fb05a39164155db26ebd0c
yijie4188 <yijie4188@gmail.com>
2023-02-14 23:52:11 +0800

    feat(builder-dts): support build dts files

--- builders/dts/src/index.ts
+++ builders/dts/src/index.ts
@@ -1,5 +1,9 @@
-import { createUseBuilderMatrix, definePlugin, resolveBuilderOpts } from '@linearite/core'
+import path from 'path'
+import * as process from 'process'
+import * as child_process from 'child_process'
 import { CompilerOptions } from 'typescript'
+
+import { createUseBuilderMatrix, definePlugin, resolveBuilderOpts } from '@linearite/core'
 
 export interface TSConfig {
   files?: string[]
@@ -32,8 +36,6 @@ const useMatrix = createUseBuilderMatrix<'dts'>(({
     console.warn('dts not support platform')
   if (conf.minify)
     console.warn('dts not support minify')
-  if (conf.sourcemap)
-    console.warn('dts not support sourcemap')
   if (conf.external)
     console.warn('dts not support external')
   if (conf.define)
@@ -44,10 +46,16 @@ const useMatrix = createUseBuilderMatrix<'dts'>(({
 
   // TODO resolve [typeVersions](https://www.typescriptlang.org/docs/handbook/declaration-files/publishing.html#version-selection-with-typesversions)
 
+  // replace `workspace.path` as '', because `tsc` command cwd is set as `workspace.path`
   return {
-    include: filedResolver('input'),
+    ...(
+      typeof conf.tsconfig === 'string'
+        ? { extends: conf.tsconfig.replace(workspace.path, '.'), }
+        : {}
+    ),
+    include: filedResolver('input').map(p => p.replace(workspace.path, '.')),
     compilerOptions: {
-      outDir: filedResolver('outfile'),
+      outDir: filedResolver('outfile').replace(workspace.path, '.'),
       declaration: true,
       declarationMap: conf.sourcemap,
       emitDeclarationOnly: true,
@@ -70,10 +78,33 @@ export default definePlugin({
       const confs = ctx.overides.calc(workspace)
       await Promise.all(confs.map(async ({ builder }) => {
         const [, conf] = resolveBuilderOpts(builder)
+        if (conf.type !== 'dts') return
+
         const matrixResolver = useMatrix(conf)
         ctx.logger.info(ctx.pluginName, 'build:item', workspace.meta.name, opts, conf)
-        await matrixResolver(workspace, async (buildOpts: TSConfig) => {
-          // TODO to build target
+        await matrixResolver(workspace, (buildOpts: TSConfig) => {
+          return new Promise((resolve, reject) => {
+            const c = child_process.spawn('npx', [
+              'tsc',
+              ...(buildOpts.extends ? ['-p', buildOpts.extends] : []),
+              ...Object.entries(buildOpts.compilerOptions).flatMap(([k, v]) => (
+                ['boolean', 'undefined'].includes(typeof v)
+                  ? (v === true ? [`--${k}`] : [])
+                  : [`--${k}`, v as string]
+              )),
+            ], {
+              stdio: 'inherit',
+              cwd: path.join(process.cwd(), workspace.path),
+            })
+            c.stdout?.pipe(process.stdout)
+            c.stderr?.pipe(process.stderr)
+            c.on('close', code => {
+              if (code === 0)
+                resolve()
+              else
+                reject(new Error(`tsc exited with code ${code}`))
+            })
+          })
         })
         ctx.logger.info(
           ctx.pluginName,
